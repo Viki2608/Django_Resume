09f923e0f6f5036868afeb754ee6e97a7e2864ed
Viki2608 <vignesh26.manoharan@gmail.com>
2022-01-11 10:33:33 +0530

    changed2

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+w1--e6g#yv#a1pgh4m_t%i_t)6b2mo$wpt)&x08vir+g_e34g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://vigneshresume.herokuapp.com/','127.0.0.1' ]
+ALLOWED_HOSTS = ['vigneshresume.herokuapp.com','127.0.0.1' ]
 
 
 # Application definition
